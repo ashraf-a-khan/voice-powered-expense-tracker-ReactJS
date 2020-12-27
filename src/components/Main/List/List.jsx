2439103ed87c67a4cb4c89ad4a30634d75294ed9
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-12-27 15:26:41 -0500

    Adds form functionality to app

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -10,12 +10,15 @@ import {
   Slide,
 } from "@material-ui/core";
 import { Delete, MoneyOff } from "@material-ui/icons";
+import { ExpenseTrackerContext } from "../../../context/context";
 
-// import { ExpenseTrackerContext } from "../../../context/context";
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
+  const { deleteTransaction } = useContext(ExpenseTrackerContext);
+
+  // console.log(globalState);
 
   const transactions = [
     {
